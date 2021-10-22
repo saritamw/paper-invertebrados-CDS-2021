2bd792021f6d2e4d521acfd2dedc66671c3c394f
saritamw <saritamw@gmail.com>
2021-10-22 09:37:58 -0600

    actualizar script de limpieza de datos

--- scripts/01_dataclean.R
+++ scripts/01_dataclean.R
@@ -1,40 +1,40 @@
-################ CDS INVERTEBRATE DATA #######################
+################ DATOS INVERTEBRADOS CDS #######################
 
-### Data exploration of invertebrate dataset and environmental variables
+## Exploración de datos de invertebrados y variables ambientales CDS
 
-### Clean the data, check for NAs and get the data ready for analysis
-### Summarized data to estimate means of temperature and humidity given the time intervals of the traps
-### See insect dataset.
+## Limpieza de datos, buscar NAs y preparar datos para el análisis
+## Resumir datos para estimar medias de temperatura y humedad en los periodos de monitoreo
+### ver los datos de invertebrados
 
-### First need to clean the updated dataset:
+## Primero hay que limpiar los datos actualizqdos:
 trap_data_raw <- read_csv("data/raw/datos_trampas_2017_21.csv")
 
-## make a complete species taxonomic list:
+## Crear una lista taxonómica de todas las especies:
 taxa_list <- select(trap_data_raw, species=especie, family=familia, order=orden) %>% 
   arrange(species, family, order) %>% 
   distinct(species, .keep_all=T)
 
-# Check to make sure all are spelled correctly:
+# Verificar que los nombres están bien escritos:
 sort(unique(taxa_list$order))
 
 
 monitor_data <- mutate(trap_data_raw,
-                       # fix column types and create new ones where needed
+                       # corregir tipos de columna y crear nuevos si es necesario
                        fecha_recogida = mdy(fecha_recogida),
                        fecha_puesta = mdy(fecha_puesta),
                        dias_monitoreo = fecha_recogida - fecha_puesta,
                        trampa_ID = paste(periodo_monitoreo, tipo_trampa, ubicacion, num_trampa_monitoreo, sep=".")) %>% 
-  # Then left-join the taxon list to fill in missing family and orders
+  # "left-join" la lista de taxones para completar familias y ordenes que falten
   left_join(taxa_list, by=c("especie"="species")) %>% 
   select(-familia, -orden) %>% 
   rename(familia = family, orden = order) %>% 
-  # finally, let's remove some old columns that we don't need:
+  # eliminamos las columnas que ya no requerimos:
   select(-(especies_monitoreo:abundancia_monitoreo)) %>% 
   mutate(monitoreo = as.numeric(as.character(monitoreo)),
          monitoreo = monitoreo-1,
          monitoreo = paste0("interv", monitoreo))
 
-## Next need to do some minor reformatting to the environmental dataset:
+## Actualizaciones de formato al archivo de datos ambientales:
 env_data <- read_csv("data/raw/datos_temp_hum_2012-2021.csv", 
                      col_types = cols(`Temperatura (zona ampliación)` = col_number(),
                                       `Humedad (zona ampliación)` = col_number())) %>% 
@@ -42,28 +42,13 @@ env_data <- read_csv("data/raw/datos_temp_hum_2012-2021.csv",
          Temp_ZA = `Temperatura (zona ampliación)`, Humedad_entrada = `Humedad (entrada)`,
          Humedad_ZA = `Humedad (zona ampliación)`) %>% 
   mutate(fecha = dmy(fecha)) %>% 
-  # add new columns:
+  # añadir columnas nuevas:
   mutate(meses = month(fecha),
          meses_nombre = month(fecha, label=T),
          dias_num = day(fecha),
          year = year(fecha))
 
-
-# # Temperature dataset
-# env_data <- read_csv("data/raw/environmental data.csv")
-# env_data <- as_tibble(env_data)
-# 
-# # Insect trap dataset
-# monitor_data <- read_csv("Data/Processed/datos_trampas_2017_20_V3.csv",
-#                          col_types = cols(fecha_puesta = col_date(format = "%m/%d/%Y"),
-#                                           fecha_recogida = col_date(format = "%m/%d/%Y"),
-#                                           dias_monitoreo = col_number(), abundancia_trampa = col_number(),
-#                                           especies_monitoreo = col_number(),
-#                                           num_especies_monitoreo = col_number(),
-#                                           abundancia_monitoreo = col_number()))
-# monitor_data <- as_tibble(monitor_data)
-
-# Check datasets
+# comprobar archivos
 names(env_data)
 head(env_data)
 str(env_data)
@@ -72,7 +57,7 @@ names(monitor_data)
 head(monitor_data)
 str(monitor_data)
 
-# Changed variables to factors
+# cambiar tipo de variables a factores
 env_data <- env_data %>% mutate_at(vars(meses, meses_nombre, year), list(factor))
 monitor_data <- monitor_data %>% mutate_at(vars(periodo_monitoreo,
                                                 monitoreo,
@@ -84,7 +69,7 @@ monitor_data <- monitor_data %>% mutate_at(vars(periodo_monitoreo,
                                                 familia,
                                                 especie), list(factor))
 
-##Creating a table with a column determining if insect species are pests
+## Crear una tabla con una columna de invertebrados que pueden ser plagas
 pest_table <- tibble(species = unique(monitor_data$especie), pest = NA)
 write.csv(pest_table, "data/raw/pest_table.csv", row.names=F)
 
@@ -94,10 +79,9 @@ monitor_data <- left_join(monitor_data, pest_table, by=c("especie"="species"))
 
 # num_trampa_monitoreo es un trampa ID por monitoreo.
 
+### Para estimar estadísticas por mes parecidas a los intervalos en los datos de insectos
 
-##### Estimate summary statistics per month similar to the intervals in the insect data ####
-
-# First, what are all the monitoring intervals?
+# Establecer intervalos de periodos de monitoreo
 interv0 <- interval(ymd("2017-05-11"), ymd("2017-12-11"))
 interv1 <- interval(ymd("2017-12-12"), ymd("2018-07-11"))
 interv2 <- interval(ymd("2018-07-12"), ymd("2019-02-11"))
@@ -110,7 +94,7 @@ interv8 <- interval(ymd("2020-10-23"), ymd("2021-01-22"))
 interv9 <- interval(ymd("2021-01-23"), ymd("2021-04-23"))
 
 
-# First create all the monitoring intervals for the environmental data:
+# Crear todos los intervalos de monitoreo para los datos ambientales:
 env_data_summ <- mutate(env_data, 
                         monitoring_interval = case_when(fecha %within% interv0 ~ "interv0",
                                                         fecha %within% interv1 ~ "interv1",
@@ -122,11 +106,11 @@ env_data_summ <- mutate(env_data,
                                                         fecha %within% interv7 ~ "interv7",
                                                         fecha %within% interv8 ~ "interv8",
                                                         fecha %within% interv9 ~ "interv9")) %>% 
-  # filter out all NA values which are the ones that didn't fall within an interval
+# Filtrar los valores NA que son los que no pertenecen a ningún intervalo
   filter(!is.na(monitoring_interval)) %>% 
   mutate(temp_avg = (Temp_entrada + Temp_ZA)/2,
          humi_avg = (Humedad_entrada + Humedad_ZA)/2,
-         # then add an exception for NAs:
+         # añadir excepción para los NAs:
          temp_avg = ifelse(is.na(Temp_entrada), Temp_ZA, temp_avg),
          temp_avg = ifelse(is.na(Temp_ZA), Temp_entrada, temp_avg),
          humi_avg = ifelse(is.na(Humedad_entrada), Humedad_ZA, humi_avg),
@@ -149,17 +133,16 @@ env_data_summ <- mutate(env_data,
 env_data_summ$days_above_50_humi
 env_data_summ$days_above_24_temp
 
-### Now join the environmental summaries to the monitoring data:
+## Unir los resúmenes ambientales a los datos de monitoreo:
 insect_env_full_data <- left_join(monitor_data, env_data_summ, by = c("monitoreo" = "monitoring_interval"))
 
 
-#### Processed dataset with summaries ####
+## Datos procesados con los resúmenes ##
 write_csv(insect_env_full_data, "data/processed/datos de insectos y temperatura final.csv")
 write_csv(env_data_clean, "data/processed/enviro_data_clean.csv")
 
 
-
-### Now prep data for the RDA analysis:
+## Preparación de datos para  el análisis de RDA:
 ##### Mediciones inividuales por TRAMPA y ABUNDANCIA de especies
 # TRAMPA sería cada fila y especies por columna con su abundancia
 
@@ -209,10 +192,5 @@ temp_hum_col <- temp_hum_col %>% group_by(trampa_ID_unico) %>% filter(row_number
 # Matrices finales, para la RDA. Matriz de parametros ambientales:
 
 write_csv(temp_hum_col, "data/processed/Matrix temperatura humedad.csv")
-
-#### Now over each monitoring period need to calculate the total number of days that
-#### were above 24C or 50% humidity
-
-
 
 
